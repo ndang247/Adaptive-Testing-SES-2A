3a12eb7752e5addbf7b96b757bc6d8df2cd3d5f5
Justin5021 <jc.wk01@gmail.com>
2021-08-27 19:41:59 +0800

    User and Maker
    - Made a basic idea for the maker and student views of their dashboard
    - Feel free to change anything

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // Pages
 import Home from 'src/pages/home/Home';
 import Login from 'src/pages/login/Login';
+import StudentDash from 'src/pages/dashboard/StudentDash';
+import MakerDash from "src/pages/dashboard/MakerDash";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/login" exact component={Login} />
+        <Route path="/student" exact component={StudentDash} />
+        <Route path="/maker" exact component={MakerDash} />
       </Switch>
     </BrowserRouter>
   );
