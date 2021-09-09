86321b02f1c0b52a55f19b01f609f9bae6090171
Cormac H <cormach4@gmail.com>
2021-09-09 15:34:13 +1000

    Setup validation for test creation fields

--- server/controllers/test.controller.js
+++ server/controllers/test.controller.js
@@ -6,6 +6,69 @@ import test from '../models/test.js';
 
 dotenv.config();
 
+
+export const createTest = async (req, res) => {
+    const result = validationResult(req);
+
+    // Return bad request on validation error
+    // If the result is not empty then there is something wrong
+    if (!result.isEmpty()) return res.status(400).json({ errors: result.errors });
+
+    const { firstName, lastName, email, password } = req.body;
+
+    try {
+        // Check to see if a user exists
+        let user = await UserModel.findOne({ email });
+
+        // If a user was already found in the database by email throw error
+        if (user) return res.status(400).json({ errors: 'Account already exists' });
+
+        // Get user avatar
+        const avatar = gravatar.url(email, {
+            s: '200',
+            d: 'mm'
+        });
+
+        // Encrypt user password
+        const hashedPassword = await hashPassword(password);
+
+        // User instance with validated data
+        user = new UserModel({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            avatar,
+            role: User,
+            dateCreated: DateTime.now(),
+            scoreIds: []
+        });
+
+        await user.save();
+
+        // Return jsonwebtoken for authorization
+        const payload = { // Create payload from mongoDB id -> note mongoDB id is _id
+            user: {
+                _id: user._id,
+                email: user.email,
+                role: user.role
+            }
+        }
+
+        // TODO: Shorten expiry time post production -> set 1h
+        jwt.sign(payload, process.env.JWT_SECRET_TOKEN, { expiresIn: 3600000 },
+            (err, token) => { // Either returns error or token
+                if (err) throw err;
+                res.status(201).json({ token });
+            });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Server Error');
+    }
+}
+
+
 export const validatePin = async (req, res) => {
     const result = validationResult(req);
 
--- server/models/test.js
+++ server/models/test.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const testSchema = mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    tite: { type: String, require: true },
+    title: { type: String, require: true },
     participantIds: { type: [mongoose.Schema.Types.ObjectId], ref: "User" },
-    maximumAttempts: { type: Number, require: true },
+    //maximumAttempts: { type: Number, require: true },
     questionIds: { type: [mongoose.Schema.Types.ObjectId], ref: "Question" },
     dateCreated: { type: Date, default: new Date() },
     expiryDate: { type: Date, require: true },
