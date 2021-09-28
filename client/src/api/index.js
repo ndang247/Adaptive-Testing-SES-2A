c02f56d79cc0630cfce13403ee3ff51c47b2eba8
ndang <nathandang73@gmail.com>
2021-09-28 17:15:46 +1000

    Fetch questions

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -14,3 +14,6 @@ export const login = (form) => API.post('/api/users/login', form);
 export const register = (form) => API.post('/api/users/register', form);
 export const hostLogin = (form) => API.post('/api/hosts/login', form);
 export const hostRegister = (form) => API.post('/api/hosts/register', form);
+
+// Exam
+export const questions = (testId, questionId) => API.get(`api/questions/${testId}/${questionId}`); 
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+import question from './question';
 
-export const reducers = combineReducers({ auth });
+export const reducers = combineReducers({ auth, question });
