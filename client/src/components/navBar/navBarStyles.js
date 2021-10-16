7ea3b36e56cd1eb1955da7723f0e53e77b8e82ad
Sacha Gilchrist <sacha.gilchrist@gmail.com>
2021-10-17 02:04:22 +1100

    Completed fundamentals of navbar
    Set up functionality and cropped the logo

--- client/src/components/navBar/navBarStyles.js
+++ client/src/components/navBar/navBarStyles.js
@@ -1,17 +1,30 @@
-import { makeStyles } from '@material-ui/styles';
+import { CenterFocusStrong } from '@material-ui/icons';
+import { makeStyles, withThemeCreator } from '@material-ui/styles';
 
 export default makeStyles(() => ({
     root: {
         flexGrow: 1,
     },
-    logo: {
-        flexGrow: 1,
-    },
+    
     appBar: {
-        background: 'transparent',
-        boxShadow: 'none'
+        background: 'black',
+        boxShadow: 'none',
     },
-    loginBtn: {
-        backgroundColor: '#5664d2',
+
+    color: {
+        border: '1px solid white',
+        textAlign: 'center',
     },
+
+    image: {
+        marginRight: '20px',
+        height: '50px',
+        display: 'block',
+        fontSize: '0',
+    },
+
+    text: {
+        lineHeight: '50px',
+        height: '50px',
+    }
 }));
